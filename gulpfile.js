d8e50fdf1398f3ee57de6b853ba0376e74669f6b
Calum Smith <hello@calum.io>
2016-03-10 12:27:20 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp         = require('gulp'),
+    rename       = require('gulp-rename'),
+    browserSync  = require('browser-sync').create(),
+    postcss      = require('gulp-postcss'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('autoprefixer'),
+    precss       = require('precss'),
+    nano         = require('gulp-cssnano'),
+    imagemin     = require('gulp-imagemin')
+    pngcrush     = require('imagemin-pngcrush'),
+    changed      = require('gulp-changed');
+
+
+/**
+ * Static server; autoprocesses SCSS files, automatically
+ * updates CSS when it changes, and reloads when HTML does.
+ */
+gulp.task('serve',['postcss'], function() {
+  browserSync.init({
+    server: './'
+    });
+
+  gulp.watch('assets/postcss/*.scss', ['postcss']);
+  gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+/**
+ * Pngcrushes PNG images in assets/img.
+ */
+gulp.task('crush', function() {
+  return gulp.src('./assets/img/src/**/*.{png,gif,jpg}')
+    .pipe(changed('./assets/img/src/**/*.{png,gif,jpg}'))
+    .pipe(imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      use: [pngcrush()]
+    }))
+    .pipe(gulp.dest('./assets/img/'));
+})
+
+/**
+ * Makes CSS from pseudo-SASS, adds appropriate vendor
+ * prefixes, writes maps, spits out to assets/css.
+ */
+gulp.task('postcss', function () {
+  return gulp.src('./assets/postcss/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(postcss([ 
+    autoprefixer({ browsers: ['last 2 versions'] }),
+    precss()
+  ]))
+  .pipe(nano())
+  .pipe(rename({extname:'.css'}))
+  .pipe(gulp.dest('./assets/css/'))
+  .pipe(browserSync.stream())
+  .pipe(sourcemaps.write('.'));
+});
+
+gulp.task('default',['serve']);
